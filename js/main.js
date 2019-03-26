04475cc834d038e161231537a61b983a7c801b4f
Daniil <42873236+DaniilD@users.noreply.github.com>
2019-03-27 03:16:44 +0500

    attractions ekb

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('.slider').slick(
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots:false,
+		arrows:false,
+		autoplay: true,
+		autoplaySpeed:3000,
+		centerMode:false,
+		centerPadding:'0px',
+		draggable:false,
+		pauseOnFocus:false,
+		pauseOnHover:false,
+		touchMove:false,
+
+	);
+
+
+});
